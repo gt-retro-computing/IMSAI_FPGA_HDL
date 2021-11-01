283d787cb8455d1b3484d3312dad9625512323f5
Khaidde <calthekhid@gmail.com>
2021-10-31 20:54:58 -0400

    Update wishbone master implementation with states

--- rtl/wb_master.sv
+++ rtl/wb_master.sv
@@ -5,8 +5,11 @@ module wb_master(
     i_clk,
 
     // S100 signals
-    i_memr,
-    i_wo_n,
+    i_s_out, // sOUT
+    i_s_inp, // sINP
+    i_dbin,  // pDBIN
+    i_wr_n,  // pWR*
+
     i_addr,
     i_do, // CPU -> Device
     o_di, // Device -> CPU
@@ -16,7 +19,6 @@ module wb_master(
     o_phantom_n,
     o_int_n,
     o_hold_n,
-    o_wr_n, // pWR*
 
     // Wishbone Signals
     o_wb_cyc,
@@ -39,17 +41,18 @@ input wire i_rst;
 input wire i_clk;
 
 // S100 Signals
-input wire i_memr;
-input wire i_wo_n;
-input wire[ADDR_LINES-1:0] i_addr;
+input wire i_s_out;
+input wire i_s_inp;
+input wire i_dbin;
+input wire i_wr_n;
 
+input wire[ADDR_LINES-1:0] i_addr;
 input wire[WIDTH-1:0] i_do;
 output reg[WIDTH-1:0] o_di;
 
 output wire o_rdy;
 output wire o_xrdy;
 output reg o_phantom_n = 1, o_int_n = 1, o_hold_n = 1;
-output reg o_wr_n;
 
 // Wishbone Signals
 output reg o_wb_cyc;
@@ -63,54 +66,64 @@ input wire i_wb_ack;
 input wire i_wb_stall;
 input wire [WIDTH-1:0] i_wb_data;
 
-assign o_rdy = o_wb_cyc;
-assign o_xrdy = o_wb_cyc;
+localparam STATE_IDLE = 0,
+           STATE_REQUEST = 1,
+           STATE_ACK = 2;
+
+reg state = STATE_IDLE;
+
+assign o_rdy = state == STATE_IDLE;
+assign o_xrdy = state == STATE_IDLE;
 
 always @(posedge i_clk) begin
   if (i_rst) begin
     o_wb_cyc <= 1'b0;
     o_wb_stb <= 1'b0;
-  end else if (o_wb_stb) begin 
-    // REQUEST Phase
+    state = STATE_IDLE;
+  end
+end
+
+always @(posedge i_clk) begin
+  if (state == STATE_IDLE) begin
+    if (!i_s_out && !i_s_inp) begin
+
+      o_wb_addr <= i_addr;
+      if (i_dbin) begin
+        o_wb_we <= 1'b0;
+      end else if (!i_wr_n) begin
+        o_wb_we <= 1'b1;
+        o_wb_data <= i_do;
+      end
+
+      if (i_dbin || !i_wr_n) begin
+        o_wb_cyc <= 1'b1;
+        o_wb_stb <= 1'b1;
+        state <= STATE_REQUEST;
+      end
+    end
+  end else if (state == STATE_REQUEST) begin
     if (!i_wb_stall) begin
       o_wb_stb <= 1'b0;
-      if (i_wb_ack)
+      state <= STATE_ACK;
+
+      if (i_wb_ack) begin
         o_wb_cyc <= 1'b0;
+
+        if (!o_wb_we) begin
+          o_di <= o_wb_data;
+        end
+        state <= STATE_IDLE;
+      end
     end
-  end else if (o_wb_cyc) begin
-    // WAIT Phase
-    if (i_wb_ack)
+  end else if (state == STATE_ACK) begin
+    if (i_wb_ack) begin
       o_wb_cyc <= 1'b0;
-  end else begin
-    // IDLE Phase
-    if (i_memr)
-      o_wb_we <= 1'b0;
-    else if (!i_wo_n)
-      o_wb_we <= 1'b1;
 
-    if (i_memr || !i_wo_n) begin
-      o_wb_cyc <= 1'b1;
-      o_wb_stb <= 1'b1;
+      if (!o_wb_we) begin
+        o_di <= o_wb_data;
+      end
+      state <= STATE_IDLE;
     end
-  end
-end
-
-always @(posedge i_clk) begin
-  if (o_wb_stb && !i_wb_stall) begin
-    o_wb_addr <= i_addr;
-  end
-end
-
-always @(posedge i_clk) begin
-  if (!o_wb_stb || !i_wb_stall) begin
-    o_wb_data <= i_do;
-  end
-end
-
-always @(posedge i_clk) begin
-  if (!i_rst && o_wb_cyc && i_wb_ack) begin 
-      o_di <= o_wb_data;
-      o_wr_n <= 1'b0;
   end
 end
 
