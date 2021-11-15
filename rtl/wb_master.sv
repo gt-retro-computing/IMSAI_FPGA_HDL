5531b513864243878cee0fc1a062d8191910aa34
Khaidde <calthekhid@gmail.com>
2021-11-14 19:59:54 -0500

    Cleanup some wb signals and add comments

--- rtl/wb_master.sv
+++ rtl/wb_master.sv
@@ -66,62 +66,77 @@ input wire i_wb_ack;
 input wire i_wb_stall;
 input wire [WIDTH-1:0] i_wb_data;
 
+// wb_master state
 localparam STATE_IDLE = 0,
            STATE_REQUEST = 1,
            STATE_ACK = 2;
 
 reg state = STATE_IDLE;
 
+// Signal to s100 master whether or not the wishbone master/slave
+// is still processing a request. Only ready if wb master is in IDLE state
 assign o_rdy = state == STATE_IDLE;
 assign o_xrdy = state == STATE_IDLE;
 
+// stb is only active while wishbone is processing a request
+assign o_wb_stb = state == STATE_REQUEST;
+
+// cyc is held high while request cycle is happening, aka state is not IDLE
+assign o_wb_cyc = state != STATE_IDLE;
+
 always @(posedge i_clk) begin
   if (i_rst) begin
-    o_wb_cyc <= 1'b0;
-    o_wb_stb <= 1'b0;
     state = STATE_IDLE;
   end
 end
 
 always @(posedge i_clk) begin
   if (state == STATE_IDLE) begin
+
+    // Initiate request if master is not executing output cycle
+    // and not executing input cycle
     if (!i_s_out && !i_s_inp) begin
 
-      o_wb_addr <= i_addr;
       if (i_dbin) begin
+        // Read request
         o_wb_we <= 1'b0;
+        
+        o_wb_addr <= i_addr;
       end else if (!i_wr_n) begin
+        // Write request
         o_wb_we <= 1'b1;
+
         o_wb_data <= i_do;
+        o_wb_addr <= i_addr;
       end
 
       if (i_dbin || !i_wr_n) begin
-        o_wb_cyc <= 1'b1;
-        o_wb_stb <= 1'b1;
+        // If write or read request, transition to REQUEST state
         state <= STATE_REQUEST;
       end
     end
   end else if (state == STATE_REQUEST) begin
     if (!i_wb_stall) begin
-      o_wb_stb <= 1'b0;
       state <= STATE_ACK;
 
+      // Handle case where slave device acks in the same cycle of ending stall
       if (i_wb_ack) begin
-        o_wb_cyc <= 1'b0;
-
+        // If request was a write, update data input on the s100 bus
         if (!o_wb_we) begin
           o_di <= o_wb_data;
         end
+
         state <= STATE_IDLE;
       end
     end
   end else if (state == STATE_ACK) begin
+    // Wait until wishbone slave acks
     if (i_wb_ack) begin
-      o_wb_cyc <= 1'b0;
-
+      // If request was a write, update data input on the s100 bus
       if (!o_wb_we) begin
         o_di <= o_wb_data;
       end
+
       state <= STATE_IDLE;
     end
   end
